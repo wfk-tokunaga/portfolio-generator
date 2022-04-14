df7c0a8dec2c950fdac8581818df1cfba2e5fd3f
Will Tokunaga <thrill.ssb@gmail.com>
2022-04-13 19:46:17 -0700

    modularized code

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-console.log("Go T-Wolves!");
+// console.log("Go T-Wolves!");
+const fs = require('fs');
+// We now have access to all of the file system methods
+const generatePage = require('./src/page-template.js');
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-console.log(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-// profileDataArgs = "hi";
+    console.log('Portflio complete! Check out index.html to see the output.');
+})
 
-// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(arg => console.log(arg))
-};
-
-printProfileData(profileDataArgs);
