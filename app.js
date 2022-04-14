97297ca65ccbb463a1acb920b08f430e92cabf5d
Will Tokunaga <thrill.ssb@gmail.com>
2022-04-14 10:24:29 -0700

    created prompt with inquirer

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-// console.log("Go T-Wolves!");
-const fs = require('fs');
-// We now have access to all of the file system methods
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+console.log(inquirer);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    }
+    ])
+    .then(answers => {
+        console.log(answers);
+    });
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// // console.log("Go T-Wolves!");
+// const fs = require('fs');
+// // We now have access to all of the file system methods
 
-    console.log('Portflio complete! Check out index.html to see the output.');
-})
+
+// const generatePage = require('./src/page-template.js');
+
+// // const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portflio complete! Check out index.html to see the output.');
+// })
 
 
